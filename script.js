90f2a06da63eab7a86131a456155c4cd8d7c6a75
MostofaKobirAdon <mostofakobiradon9@gmail.com>
2025-08-28 10:03:19 +0600

    added heart icon functionalities

---
+++ script.js
@@ -0,0 +1,18 @@
+// functions
+
+
+// heart counting
+
+const hearts = document.getElementsByClassName('heart');
+
+for (let heart of hearts) {
+    heart.addEventListener('click', function () {
+        let heartNum = document.getElementById("heart-number")
+        heartNum.innerText++;
+    })
+}
+
+// call btn functionalities
+
+
+
